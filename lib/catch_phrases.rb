85b7d2f4e13420d49e8b57f90ce983d5d3dde2d2
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-22 15:26:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,19 +1,19 @@
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  phrase
+  # phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  status
+  # status
 end
 
 def link 
   words = "It's Dangerous To Go Alone! Take This."
   puts words
-  words
+  # words
 end
 
 def all_phrases
